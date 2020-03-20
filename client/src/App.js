3206d517dbb8f0c54bf4c7c1feffe2d8b4876c97
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-20 17:27:18 -0600

    Removed BrowserRouter from App.js. Created a PrivateRoute component and imported/rendered it in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
+
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
+    <div className="App">
+      <Route exact path="/" component={Login} />
+      {/* 
+        Build a PrivateRoute component that will 
+        display BubblePage when you're authenticated 
+      */}
+      <Switch>
+        <PrivateRoute exact path="/api/bubble-page" component={BubblePage} />
+        {/* <Route path="/api/login" component={Login} /> */}
+      </Switch>
+    </div>
   );
 }
 
