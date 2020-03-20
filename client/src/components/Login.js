5d586fcc54b8d41f72d653b3b94f51b6cd4d6e03
Tyler Alsop <tylerjalsop@gmail.com>
2020-03-20 17:45:30 -0600

    Created an axiosWithAuth component and imported into login.js. Built login form and added some styling. Login works! Token is returned.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,72 @@
-import React from "react";
+import React, { useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState(
+    {
+    username: "",
+    password: ""
+    }
+  );
+
+  const handleChange = e => {
+    setCredentials(
+      {
+        ...credentials,
+        [e.target.name]: e.target.value
+      }
+    );
+  };
+
+  const login = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+    .post("/api/login", credentials)
+    .then(res => {
+      console.log("Login Response", res);
+      localStorage.setItem("token", res.data.payload);
+      props.history.push("/api/colors");
+    })
+    .catch(err => {
+      console.log("Login Error", err);
+    });
+  }
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="login-form">
+      <h1 className="login-form-header">Welcome to the Bubble App!</h1>
+      <div>
+        <form onSubmit={login} className="login-form">
+          <label id="label" className="username-label">
+            Username:
+              <input 
+                id="input" 
+                type="text"
+                name="username"
+                onChange={handleChange} 
+              />
+          </label>
+          <label id="label" className="password-label">
+            Password:
+              <input 
+                id="input" 
+                type="text"
+                name="password"
+                onChange={handleChange} 
+              />
+          </label>
+          <button className="login-button">Login</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
 export default Login;
+
+
